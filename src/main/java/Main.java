4eb6f5e4f5667db84e21a33ca80d5e0d25b90e1e
vikeedough <156529589+vikeedough@users.noreply.github.com>
2025-02-18 17:43:19 +0800

    Remove unnecessary lines

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,11 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class Main extends Application {
     private Duke duke = new Duke();
@@ -33,15 +23,5 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-
-//    private void handleUserInput() {
-//        String userText = userInput.getText();
-//        String dukeText = duke.getResponse(userText);
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(userText, userImage),
-//                DialogBox.getDukeDialog(dukeText, dukeImage)
-//        );
-//        userInput.clear();
-//    }
 
 }
